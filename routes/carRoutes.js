4340d6a33a780f91b2b80af6dbfdccc45f463e11
Muin Chowdhury <muin.bd@gmail.com>
2026-05-18 21:31:35 +0600

    implement get single car api

--- routes/carRoutes.js
+++ routes/carRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 
 const router = express.Router();
 
@@ -37,6 +38,32 @@ router.get("/", async (req, res) => {
 
     res.status(500).send({
       message: "Failed to fetch cars",
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const carsCollection = db.collection("cars");
+
+    const id = req.params.id;
+
+    const car = await carsCollection.findOne({
+      _id: new ObjectId(id),
+    });
+
+    if (!car) {
+      return res.status(404).send({
+        message: "Car not found",
+      });
+    }
+
+    res.send(car);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).send({
+      message: "Failed to fetch car",
     });
   }
 });
