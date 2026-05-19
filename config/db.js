cdde2ba9875f82fabd4b53806cf07114860607f6
muinphero <muinphero@gmail.com>
2026-05-19 21:03:10 +0600

    Fix booking routes, booking count logic, car routes and API improvements

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
+
     strict: true,
+
     deprecationErrors: true,
   },
 });
